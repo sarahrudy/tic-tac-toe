e7a89e94a17c9c3115eda9eb72a0f4e105d5d5bb
Sarah <sarahrudy@gmail.com>
2021-06-12 00:33:41 -0600

    Create a player.js file

---
+++ player.js
@@ -0,0 +1,15 @@
+class Player {
+  constructor(id, token, wins) {
+    this.id = id;
+    this.token = token;
+    this.wins = [];
+  }
+}
+
+saveWinsToStorage() {
+  // push win into wins array
+}
+
+retrieveWinsFromStorage() {
+  // be able to keep score until a "reset scores" button is pushed
+}
