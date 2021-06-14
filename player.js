e754aab1ef6832226f4078655ce8fa8fe02ce179
Sarah <sarahrudy@gmail.com>
2021-06-13 19:31:15 -0600

    Add properties to constructor

--- player.js
+++ player.js
@@ -1,16 +1,18 @@
 class Player {
-  constructor(id, icon) {
+  constructor(id, icon, currentTurn) {
     this.id = id;
     this.icon = icon;
+    this.currentTurn = currentTurn;
     this.wins = [];
     this.winner = false;
   }
-}
 
-saveWinsToStorage() {
+
+  saveWinsToStorage() {
   // push win into wins array
-}
+  }
 
-retrieveWinsFromStorage() {
+  retrieveWinsFromStorage() {
   // be able to keep score until a "reset scores" button is pushed
+  }
 }
