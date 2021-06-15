d6682754e4716aa649c0d671bb61a01f1ac87754
Sarah <sarahrudy@gmail.com>
2021-06-15 13:43:26 -0600

    Revert methods and create reset() method

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 class Game {
   constructor() {
-    this.player1 = new Player("X", "❌");
+    this.player1 = new Player("X", "𝗫");
     this.player2 = new Player("O", "◎");
     this.totalPlays = 0;
     this.currentPlayer = this.player1;
@@ -8,13 +8,10 @@ class Game {
     this.draw = false;
   }
 
-  playerMove() {
+  playerMove(click) {
     this.totalPlays++
-    if (this.currentPlayer === this.player1 || this.player2) {
-      for (var i = 0; i < this.boardDisplay.length; i++) {
-        if (this.boardDisplay[i] === event.target.id)
-          this.boardDisplay[i] = this.currentPlayer.id
-      }
+    if(this.currentPlayer) {
+    this.boardDisplay.splice(click, 1, this.currentPlayer.id)
     }
   }
 
@@ -27,44 +24,45 @@ class Game {
   }
 
   evaluateBoard() {
-    this.checkWinConditions(this.player1)
-    this.checkWinConditions(this.player2)
+  if (this.totalPlays >= 5 && this.totalPlays <= 9) {
+    this.checkWinConditions()
+  } else if (this.totalPlays === 9 && !this.hasWinner) {
+    this.draw = true
   }
-
+}
 
   checkWinConditions() {
-    for (var i = 0; i < this.boardDisplay.length; i++) {
-      if (this.boardDisplay[0] === this.boardDisplay[1] && this.boardDisplay[1] === this.boardDisplay[2] && this.boardDisplay[2] === this.currentPlayer.id) {
-        this.winner();
-      } else if (this.boardDisplay[3] === this.boardDisplay[4] && this.boardDisplay[4] === this.boardDisplay[5] && this.boardDisplay[5] === this.currentPlayer.id) {
-        this.winner();
-      } else if (this.boardDisplay[6] === this.boardDisplay[7] && this.boardDisplay[7] === this.boardDisplay[8] && this.boardDisplay[8] === this.currentPlayer.id) {
-        this.winner();
-      } else if (this.boardDisplay[0] === this.boardDisplay[3] && this.boardDisplay[3] === this.boardDisplay[6] && this.boardDisplay[6] === this.currentPlayer.id) {
-        this.winner();
-      } else if (this.boardDisplay[1] === this.boardDisplay[4] && this.boardDisplay[4] === this.boardDisplay[7] && this.boardDisplay[7] === this.currentPlayer.id) {
-        this.winner();
-      } else if (this.boardDisplay[2] === this.boardDisplay[5] && this.boardDisplay[5] === this.boardDisplay[8] && this.boardDisplay[8] === this.currentPlayer.id) {
-        this.winner();
-      } else if (this.boardDisplay[0] === this.boardDisplay[4] && this.boardDisplay[4] === this.boardDisplay[8] && this.boardDisplay[8] === this.currentPlayer.id) {
-        this.winner();
-      } else if (this.boardDisplay[2] === this.boardDisplay[4] && this.boardDisplay[4] === this.boardDisplay[6] && this.boardDisplay[6] === this.currentPlayer.id) {
-        this.winner();
-      } else if (this.totalPlays === 9) {
-        this.drawGame = true;
-      }
+    if (this.boardDisplay[0] === this.boardDisplay[1] && this.boardDisplay[1] === this.boardDisplay[2] && this.boardDisplay[2] === this.currentPlayer.id) {
+      this.updateWinner();
+    } else if (this.boardDisplay[3] === this.boardDisplay[4] && this.boardDisplay[4] === this.boardDisplay[5] && this.boardDisplay[5] === this.currentPlayer.id) {
+      this.updateWinner();
+    } else if (this.boardDisplay[6] === this.boardDisplay[7] && this.boardDisplay[7] === this.boardDisplay[8] && this.boardDisplay[8] === this.currentPlayer.id) {
+      this.updateWinner();
+    } else if (this.boardDisplay[0] === this.boardDisplay[3] && this.boardDisplay[3] === this.boardDisplay[6] && this.boardDisplay[6] === this.currentPlayer.id) {
+      this.updateWinner();
+    } else if (this.boardDisplay[1] === this.boardDisplay[4] && this.boardDisplay[4] === this.boardDisplay[7] && this.boardDisplay[7] === this.currentPlayer.id) {
+      this.updateWinner();
+    } else if (this.boardDisplay[2] === this.boardDisplay[5] && this.boardDisplay[5] === this.boardDisplay[8] && this.boardDisplay[8] === this.currentPlayer.id) {
+      this.updateWinner();
+    } else if (this.boardDisplay[0] === this.boardDisplay[4] && this.boardDisplay[4] === this.boardDisplay[8] && this.boardDisplay[8] === this.currentPlayer.id) {
+      this.updateWinner();
+    } else if (this.boardDisplay[2] === this.boardDisplay[4] && this.boardDisplay[4] === this.boardDisplay[6] && this.boardDisplay[6] === this.currentPlayer.id) {
+      this.updateWinner();
+    } else if (this.totalPlays === 9) {
+      this.reset()
+      this.drawGame = true;
     }
   }
 
-  winner() {
-    if (this.currentPlayer === this.player1) {
-      this.player1.isWinner = true
-      this.player1.tallyWin()
-    } else if (this.currentPlayer === this.player2) {
-      this.player2.isWinner = true
-      this.player2.tallyWin()
-    }
+  updateWinner() {
+    this.currentPlayer.isWinner = true
+    this.currentPlayer.tallyWin()
   }
-  // A way to reset the Game’s board to begin a new game
-  // A timeout is used after a completed game to reset the board.
+
+  reset() {
+    this.boardDisplay = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+    this.totalPlays = 0
+    this.player1.isWinner = false
+    this.player2.isWinner = false
+  }
 }
