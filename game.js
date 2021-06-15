7319b9bd22d5bb4e4f5e89eaef27946807ff1e76
Sarah <sarahrudy@gmail.com>
2021-06-14 18:52:03 -0600

    Add checkWinConditions method

--- game.js
+++ game.js
@@ -6,8 +6,11 @@ class Game {
     this.currentPlayer = this.player1;
     this.boardDisplay = ["", "", "", "", "", "", "", "", ""];
     this.hasWinner = false;
+    this.draw = false;
   }
 
+  // A Game should include:
+  // A way to reset the Game’s board to begin a new game
 
   playerMove(player, cell) {
     this.totalPlays++
@@ -17,65 +20,84 @@ class Game {
     }
   }
 
-// use this one if you end up removing this.currentTurn from Player properties in constructor
-// also remove currentTurn paramater and third argument in this.player1/this.player2 in game class
-  // playerMove(player, cell) {
-  //   this.totalPlays++
-  //   if(this.currentPlayer) {
-  //   this.boardDisplay.splice(Number(cell), 1, player.id)
-  //   this.updateTurn()
-  //   }
-  // }
-
   switchPlayer(player) {
     this.currentPlayer = player
   }
 
+// use toggle for this!
   updateTurn() {
     if (this.player1.currentTurn) {
       this.player1.currentTurn = false
       this.player2.currentTurn = true
       this.switchPlayer(this.player2)
+      // return "it's player 2's turn" message here
     } else if (this.player2.currentTurn) {
       this.player1.currentTurn = true
       this.player2.currentTurn = false
       this.switchPlayer(this.player1)
-      }
+      // return "it's player 1's turn" message here
     }
   }
 
-  //have to be able to check them against the possible win conditions
-  //parse through arrays with for loop
-  // evaluateBoard() {
-  //   // logic to check number of total plays
-  //   // if total plays is >= 5 && <= 9, check board against winning combinations
-  //   // if totalPlays = 9 and no winning coditions are met, then it's a draw
-  //
-  // }
+  evaluateBoard() {
+    if (this.totalPlays >= 5 && this.totalPlays <= 9) {
+      this.checkWinConditions()
+      // return "winning message" here
+    } else if (this.totalPlays === 9 && !this.hasWinner) {
+      this.draw = true
+      // return "draw message" here
+    }
+  }
 
-// draw() {
-//   if (this.totalPlays === 9 && )
-// }
-winner() {
-var winningCells = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-];
-this.hasWinner = true
-player.wins++
+  checkWinConditions() {
+    if(this.boardDisplay[0].includes(this.currentPlayer.id) &&
+      this.boardDisplay[1].includes(this.currentPlayer.id) && this.boardDisplay[2].includes(this.currentPlayer.id))
+      ||
+      (this.boardDisplay[3].includes(this.currentPlayer.id) && this.boardDisplay[4].includes(this.currentPlayer.id) && this.boardDisplay[5].includes(this.currentPlayer.id))
+      ||
+      (this.boardDisplay[6].includes(this.currentPlayer.id) && this.boardDisplay[7].includes(this.currentPlayer.id) && this.boardDisplay[8].includes(this.currentPlayer.id))
+      ||
+      (this.boardDisplay[0].includes(this.currentPlayer.id) && this.boardDisplay[3].includes(this.currentPlayer.id) && this.boardDisplay[6].includes(this.currentPlayer.id))
+      ||
+      (this.boardDisplay[1].includes(this.currentPlayer.id) && this.boardDisplay[4].includes(this.currentPlayer.id) && this.boardDisplay[7].includes(this.currentPlayer.id))
+      ||
+      (this.boardDisplay[2].includes(this.currentPlayer.id) && this.boardDisplay[5].includes(this.currentPlayer.id) && this.boardDisplay[8].includes(this.currentPlayer.id))
+      ||
+      (this.boardDisplay[0].includes(this.currentPlayer.id) && this.boardDisplay[4].includes(this.currentPlayer.id) && this.boardDisplay[8].includes(this.currentPlayer.id))
+      ||
+      (this.boardDisplay[2].includes(this.currentPlayer.id) && this.boardDisplay[4].includes(this.currentPlayer.id) && this.boardDisplay[6].includes(this.currentPlayer.id))
+        this.winner();
+      }
+
+  winner() {
+    this.hasWinner = true
+    this.currentPlayer.tallyWin()
+  }
+
 }
 
-//
+  // checkWinConditions() {
+  //   var winConditions = [
+  //     [0,1,2],
+  //     [3,4,5],
+  //     [6,7,8],
+  //     [0,3,6],
+  //     [1,4,7],
+  //     [2,5,8],
+  //     [0,4,8],
+  //     [2,4,6]
+  //   ]
+  // for (var i = 0; i < winConditions.length ; i++)
+  //   if(winConditions[i][0] === this.currentPlayer.id && winConditions[i][1] === this.currentPlayer.id && winConditions[i][2] === this.currentPlayer.id)
+  //     this.hasWinner = true
+  //     this.currentPlayer.tallyWin()
+  //     }
+  //   }
+
 
 // if buttons are selected by the X or O in the previous combinations, game is over and a winner is declared
-// if X wins, increase .x-num-of-wins by 1
-// if O wins, increase .o-num-of-wins by 1
+// if X wins, increase .x-num-of-wins by 1 (DOM)
+// if O wins, increase .o-num-of-wins by 1 (DOM)
 
 // function draw() {
 //   if (!boardDisplay.includes(winnerWinnerChickenDinner[0], winnerWinnerChickenDinner[1], winnerWinnerChickenDinner[2], winnerWinnerChickenDinner[3], winnerWinnerChickenDinner[4], winnerWinnerChickenDinner[5], winnerWinnerChickenDinner[6], winnerWinnerChickenDinner[7], winnerWinnerChickenDinner[8])
