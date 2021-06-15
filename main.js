2fcc2ba7c7721acbaf13c505619ebb7594a775ea
Sarah <sarahrudy@gmail.com>
2021-06-14 20:11:21 -0600

    Add query selector, event listener, and function for laodPage

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 var game = new Game() ;
 
-// QUERY SELECTORS
-// window.onlaod = blankBoard
+// window.onlaod = boardDisplay
+var buttons = document.querySelectorAll('button')
+var gameplayText = document.querySelector('.gameplay-text')
+
+window.addEventListener('load', loadPage)
 
 
-// EVENT LISTENERS
+function loadPage() {
+  gameplayText.innerText = `YOUR TURN, ${game.player1.icon}!`
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+      placeIcon(event)
+    });
+  }
+}
 
 
-// // FUNCTIONS
-// function blankBoard() {
-//   currentTurn =
-// }
+function placeIcon(event) {
+  event.preventDefault()
+  if (event.target.classList.contains('cell')) {
+    game.playerMove()
+  }
+}
+
+
+// if(.cell-0.contains("X"))
